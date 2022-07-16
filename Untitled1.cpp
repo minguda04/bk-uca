c3c49e32d60fca501f0938b529faaa0460b9ad10
strain7626 <109017712+minhae123@users.noreply.github.com>
2022-07-16 14:34:40 +0900

    2442번 문제풀이

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,23 @@
 #include <stdio.h>
-
-
-
-
 int main() {
-	printf("hello world");
+	int i, j, c;
+	int n;
+	scanf("%d", &n);
+	
+	for(i = 1; i <=n; i++)
+	{
+		for(j = 1; j <= n-i; j++){
+			printf(" ");
+		}	
+		for(c = 1; c <= 2*i-1; c++){
+			printf("*");
+		}
+		printf("\n");
+	}	
+
+
+
+
 	return 0;
 }
 
